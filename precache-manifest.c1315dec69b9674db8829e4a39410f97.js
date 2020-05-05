648f560c974da35a180b2c2a96d183b0fd1ea822
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-05-05 22:19:39 +0300

    Updates

---
+++ precache-manifest.c1315dec69b9674db8829e4a39410f97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72759bf83078ac71d4b49638dcf2ba85",
+    "url": "/React_Playground_2/index.html"
+  },
+  {
+    "revision": "bbb4cf0b7e62cd48e923",
+    "url": "/React_Playground_2/static/css/main.a83b4706.chunk.css"
+  },
+  {
+    "revision": "05112136cfe7306f664f",
+    "url": "/React_Playground_2/static/js/2.30f2a3ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Playground_2/static/js/2.30f2a3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb4cf0b7e62cd48e923",
+    "url": "/React_Playground_2/static/js/main.d691f16d.chunk.js"
+  },
+  {
+    "revision": "4fbdf8ef445b5020e9e7",
+    "url": "/React_Playground_2/static/js/runtime-main.48f79fae.js"
+  }
+]);
